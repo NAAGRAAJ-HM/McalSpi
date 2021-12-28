416c70f4a4908d3c0543de890dd81f52a538ab09
root <nagaraja.hm.0001@gmail.com>
2021-12-29 00:46:40 +0100

    Update: submodule Spi

---
+++ LATEST/Spi.cpp
@@ -0,0 +1,7 @@
+#include "Spi.h"
+
+class_Spi Spi;
+
+void class_Spi::InitFunction(void){
+}
+
---
+++ LATEST/Spi.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class class_Spi{
+   public:
+      void InitFunction(void);
+};
+
+extern class_Spi Spi;
+
