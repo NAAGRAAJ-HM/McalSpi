9662aaa2dd1d020a56c41ec7b90371181cdbd4c2
root <nagaraja.hm.0001@gmail.com>
2022-01-18 09:20:16 +0100

    Update: submodule Spi

--- LATEST/Spi.cpp
+++ LATEST/Spi.cpp
@@ -5,3 +5,45 @@ class_Spi Spi;
 FUNC(void, SPI_CODE) class_Spi::InitFunction(void){
 }
 
+FUNC(void, SPI_CODE) class_Spi::DeInitFunction(void){
+}
+
+FUNC(void, SPI_CODE) class_Spi::WriteIB(void){
+}
+
+FUNC(void, SPI_CODE) class_Spi::AsyncTransmit(void){
+}
+
+FUNC(void, SPI_CODE) class_Spi::ReadIB(void){
+}
+
+FUNC(void, SPI_CODE) class_Spi::SetupEB(void){
+}
+
+FUNC(void, SPI_CODE) class_Spi::GetStatus(void){
+}
+
+FUNC(void, SPI_CODE) class_Spi::GetJobResult(void){
+}
+
+FUNC(void, SPI_CODE) class_Spi::GetSequenceResult(void){
+}
+
+FUNC(void, SPI_CODE) class_Spi::GetVersionInfo(void){
+}
+
+FUNC(void, SPI_CODE) class_Spi::SyncTransmit(void){
+}
+
+FUNC(void, SPI_CODE) class_Spi::GetHWUnitStatus(void){
+}
+
+FUNC(void, SPI_CODE) class_Spi::Cancel(void){
+}
+
+FUNC(void, SPI_CODE) class_Spi::SetAsyncMode(void){
+}
+
+FUNC(void, SPI_CODE) class_Spi::MainFunction(void){
+}
+
--- LATEST/Spi.h
+++ LATEST/Spi.h
@@ -5,7 +5,21 @@
 
 class class_Spi{
    public:
-      FUNC(void, SPI_CODE) InitFunction(void);
+      FUNC(void, SPI_CODE) InitFunction      (void);
+      FUNC(void, SPI_CODE) DeInitFunction    (void);
+      FUNC(void, SPI_CODE) WriteIB           (void);
+      FUNC(void, SPI_CODE) AsyncTransmit     (void);
+      FUNC(void, SPI_CODE) ReadIB            (void);
+      FUNC(void, SPI_CODE) SetupEB           (void);
+      FUNC(void, SPI_CODE) GetStatus         (void);
+      FUNC(void, SPI_CODE) GetJobResult      (void);
+      FUNC(void, SPI_CODE) GetSequenceResult (void);
+      FUNC(void, SPI_CODE) GetVersionInfo    (void);
+      FUNC(void, SPI_CODE) SyncTransmit      (void);
+      FUNC(void, SPI_CODE) GetHWUnitStatus   (void);
+      FUNC(void, SPI_CODE) Cancel            (void);
+      FUNC(void, SPI_CODE) SetAsyncMode      (void);
+      FUNC(void, SPI_CODE) MainFunction      (void);
 };
 
 extern class_Spi Spi;
