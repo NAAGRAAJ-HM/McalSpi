749f9e81e27a302248521973c0dd3df3ee335b61
root <nagaraja.hm.0001@gmail.com>
2022-02-25 12:07:40 +0100

    Update: submodule Spi

--- LATEST/Spi_EcuM.h
+++ LATEST/Spi_EcuM.h
@@ -27,8 +27,8 @@ class interface_Spi_EcuM : public interface_EcuM_Client{
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-      FUNC(void, SPI_CODE) InitFunction   (void);
-      FUNC(void, SPI_CODE) DeInitFunction (void);
+      virtual FUNC(void, SPI_CODE) InitFunction   (void) = 0;
+      virtual FUNC(void, SPI_CODE) DeInitFunction (void) = 0;
 };
 
 /*****************************************************/
