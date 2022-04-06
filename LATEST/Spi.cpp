0141eb5232f4d0fda97862b8e30492f69c8c95fb
root <nagaraja.hm.0001@gmail.com>
2022-04-06 13:00:07 +0200

    Update: AUTOSAR basic software micro-controller abstraction module Spi

--- LATEST/Spi.cpp
+++ LATEST/Spi.cpp
@@ -14,18 +14,18 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define SPI_AR_RELEASE_MAJOR_VERSION                                           4
-#define SPI_AR_RELEASE_MINOR_VERSION                                           3
+#define SPI_AR_RELEASE_VERSION_MAJOR                                           4
+#define SPI_AR_RELEASE_VERSION_MINOR                                           3
 
 /******************************************************************************/
 /* MACROS                                                                     */
 /******************************************************************************/
-#if(SPI_AR_RELEASE_MAJOR_VERSION != STD_AR_RELEASE_MAJOR_VERSION)
-   #error "Incompatible SPI_AR_RELEASE_MAJOR_VERSION!"
+#if(SPI_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible SPI_AR_RELEASE_VERSION_MAJOR!"
 #endif
 
-#if(SPI_AR_RELEASE_MINOR_VERSION != STD_AR_RELEASE_MINOR_VERSION)
-   #error "Incompatible SPI_AR_RELEASE_MINOR_VERSION!"
+#if(SPI_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible SPI_AR_RELEASE_VERSION_MINOR!"
 #endif
 
 /******************************************************************************/
@@ -63,8 +63,10 @@ CONSTP2VAR(infSchMClient, SPI_VAR, SPI_CONST) gptrinfSchMClient_Spi = &Spi;
 /******************************************************************************/
 VAR(module_Spi, SPI_VAR) Spi(
    {
-         0x0000
-      ,  0xFFFF
+         SPI_AR_RELEASE_VERSION_MAJOR
+      ,  SPI_AR_RELEASE_VERSION_MINOR
+      ,  0x00
+      ,  0xFF
       ,  0x01
       ,  '0'
       ,  '1'
