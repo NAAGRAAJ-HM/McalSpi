c270326831e4b54e51afc7d688a4dfa7047ce02a
root <nagaraja.hm.0001@gmail.com>
2022-04-07 09:50:39 +0200

    Update: AUTOSAR basic software micro-controller abstraction module Spi

--- LATEST/Spi.cpp
+++ LATEST/Spi.cpp
@@ -96,8 +96,14 @@ FUNC(void, SPI_CODE) module_Spi::InitFunction(
 #endif
       }
       else{
+         if(STD_LOW){
 // check lptrCfgModule for memory faults
-// use PBcfg_Spi as back-up configuration
+            lptrCfg = lptrCfgModule;
+         }
+         else{
+// use PBcfg_CanIf as back-up configuration
+            lptrCfg = PBcfg_CanIf;
+         }
       }
       IsInitDone = E_OK;
 #if(STD_ON == Spi_InitCheck)
