2ce36aafb53f030892b79f449b1398749fc0edca
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-04 09:01:39 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Spi

--- LATEST/Spi.cpp
+++ LATEST/Spi.cpp
@@ -113,7 +113,7 @@ FUNC(void, SPI_CODE) module_Spi::InitFunction(
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  SPI_E_UNINIT
       );
 #endif
    }
@@ -133,7 +133,7 @@ FUNC(void, SPI_CODE) module_Spi::DeInitFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  SPI_E_UNINIT
       );
 #endif
    }
@@ -152,7 +152,7 @@ FUNC(void, SPI_CODE) module_Spi::MainFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  SPI_E_UNINIT
       );
 #endif
    }
