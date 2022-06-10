e765e3693b30ec37a61ca5d6ad130b1640c0faac
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-10 13:33:48 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Spi

--- LATEST/Spi.cpp
+++ LATEST/Spi.cpp
@@ -73,10 +73,6 @@ CONSTP2VAR(infSchMClient, SPI_VAR, SPI_CONST) gptrinfSchMClient_Spi = &Spi;
 /******************************************************************************/
 VAR(module_Spi, SPI_VAR) Spi(
    {
-#if(STD_ON == _ReSIM)
-         "Spi",
-#else
-#endif
          SPI_AR_RELEASE_VERSION_MAJOR
       ,  SPI_AR_RELEASE_VERSION_MINOR
       ,  0x00
