90a4d2e3a3a5e4c8105ce130e718f6ccd9ecf797
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:05:18 +0200

    Update: AUTOSAR basic software micro-controller abstraction driver module for serial peripheral interface communication protocol stack

--- VirtualMachine_LATEST/McalSpi.cpp
+++ VirtualMachine_LATEST/McalSpi.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* File   : McalSpi.cpp                                                           */
+/* File   : McalSpi.cpp                                                       */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -13,8 +13,8 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define MCALSPI_AR_RELEASE_VERSION_MAJOR                                           4
-#define MCALSPI_AR_RELEASE_VERSION_MINOR                                           3
+#define MCALSPI_AR_RELEASE_VERSION_MAJOR                                       4
+#define MCALSPI_AR_RELEASE_VERSION_MINOR                                       3
 
 /******************************************************************************/
 /* MACROS                                                                     */
