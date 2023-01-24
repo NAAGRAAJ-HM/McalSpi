d069ebe01ea349c8942c413d01ff9a0eb18ec400
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:52:15 +0100

    Update: AUTOSAR basic software micro-controller abstraction driver module for serial peripheral interface communication protocol stack

---
+++ InfineonTLE9879QXA40_R2023/infMcalSpi_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infMcalSpi_ServiceNvM.hpp                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTMCALSPI_AR_RELEASE_VERSION_MAJOR                                  4
+#define CONSTMCALSPI_AR_RELEASE_VERSION_MINOR                                  3
+
+#define Const_DefaultInit_ConstMcalSpi                                                                           \
+   ,  {CONSTMCALSPI_AR_RELEASE_VERSION_MAJOR, CONSTMCALSPI_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
